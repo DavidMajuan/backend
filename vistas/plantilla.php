99c4718e47091357dbf3de7fa3fd91bfd50963d3
DavidMajuan <davidmajuanpintado@gmail.com>
2019-07-22 10:50:57 -0500

    se agrego dinamismo de iconos, color, estilo

--- vistas/plantilla.php
+++ vistas/plantilla.php
@@ -7,70 +7,103 @@
 
   <link rel="icon" href="vistas/img/plantilla/icono.png">
 
-  <!-- Tell the browser to be responsive to screen width -->
   <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
+  
+  <!--=====================================
+  PLUGINS DE CSS
+  ======================================-->
   <!-- Bootstrap 3.3.7 -->
   <link rel="stylesheet" href="vistas/bower_components/bootstrap/dist/css/bootstrap.min.css">
+
   <!-- Font Awesome -->
   <link rel="stylesheet" href="vistas/bower_components/font-awesome/css/font-awesome.min.css">
+
   <!-- Ionicons -->
   <link rel="stylesheet" href="vistas/bower_components/Ionicons/css/ionicons.min.css">
+
   <!-- Theme style -->
   <link rel="stylesheet" href="vistas/dist/css/AdminLTE.min.css">
+  
   <link rel="stylesheet" href="vistas/dist/css/skins/skin-blue.min.css">
+
   <!-- iCheck -->
   <link rel="stylesheet" href="vistas/plugins/iCheck/square/blue.css">
+
   <!-- Morris chart -->
   <link rel="stylesheet" href="vistas/bower_components/morris.js/morris.css">
+
    <!-- jvectormap -->
   <link rel="stylesheet" href="vistas/bower_components/jvectormap/jquery-jvectormap.css">
-
- 
 
   <!-- Google Font -->
   <link rel="stylesheet"
         href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,600,700,300italic,400italic,600italic">
 
-  <!-- REQUIRED JS SCRIPTS -->
+   <!-- Bootstrap Color Picker -->
+  <link rel="stylesheet" href="vistas/bower_components/bootstrap-colorpicker/dist/css/bootstrap-colorpicker.min.css">
+
+  <!-- bootstrap slider -->
+  <link rel="stylesheet" href="vistas/plugins/bootstrap-slider/slider.css">
+
+  <!--=====================================
+  CSS PERSONALIZADO
+  ======================================-->
+
+  <link rel="stylesheet" href="vistas/css/plantilla.css">
+
+   <link rel="stylesheet" href="vistas/css/slide.css">
+
+
+  <!--=====================================
+  PLUGINS DE JAVASCRIPT
+  ======================================-->
 
   <!-- jQuery 3 -->
   <script src="vistas/bower_components/jquery/dist/jquery.min.js"></script>
+
+  <!-- jQuery UI 1.11.4 -->
+  <script src="vistas/bower_components/jquery-ui/jquery-ui.min.js"></script>
+
   <!-- Bootstrap 3.3.7 -->
   <script src="vistas/bower_components/bootstrap/dist/js/bootstrap.min.js"></script>
+
   <!-- AdminLTE App -->
   <script src="vistas/dist/js/adminlte.min.js"></script>
-  <!-- iCheck -->
+
+  <!-- iCheck http://icheck.fronteed.com/-->
   <script src="vistas/plugins/iCheck/icheck.min.js"></script>
+
     <!-- Morris.js charts -->
   <script src="vistas/bower_components/raphael/raphael.min.js"></script>
+
   <script src="vistas/bower_components/morris.js/morris.min.js"></script>
+
   <!-- jQuery Knob Chart -->
   <script src="vistas/bower_components/jquery-knob/dist/jquery.knob.min.js"></script>
+
   <!-- jvectormap -->
   <script src="vistas/plugins/jvectormap/jquery-jvectormap-1.2.2.min.js"></script>
+
   <script src="vistas/plugins/jvectormap/jquery-jvectormap-world-mill-en.js"></script>
+
   <!-- ChartJS -->
   <script src="vistas/bower_components/Chart.js/Chart.js"></script>
 
-  <script>
-    $(function () {
-      $('input').iCheck({
-        checkboxClass: 'icheckbox_square-blue',
-        radioClass: 'iradio_square-blue',
-        increaseArea: '20%' // optional
-      });
-      /* jQueryKnob */
-      $('.knob').knob();
-      /* SideBar Menu */
-      $('.sidebar-menu').tree();
-    });
-    </script>
+  <!-- SweetAlert 2 https://sweetalert2.github.io/-->
+  <script src="vistas/plugins/sweetalert2/sweetalert2.all.js"></script>
+
+  <!-- bootstrap color picker https://farbelous.github.io/bootstrap-colorpicker/v2/-->
+  <script src="vistas/bower_components/bootstrap-colorpicker/dist/js/bootstrap-colorpicker.min.js"></script>
+
+  <!-- Bootstrap slider http://seiyria.com/bootstrap-slider/-->
+  <script src="vistas/plugins/bootstrap-slider/bootstrap-slider.js"></script>
 
 </head>
 
 
 
-<body class="hold-transition skin-blue sidebar-mini login-page">
+
+<body class="hold-transition skin-blue sidebar-collapse sidebar-mini login-page">
 
 <?php
 
@@ -136,6 +169,15 @@ session_start();
  
   
  ?>
+
+
+ <!--=====================================
+JS PERSONALIZADO
+======================================-->
+
+<script src="vistas/js/plantilla.js"></script>
+<script src="vistas/js/gestorComercio.js"></script>
+<script src="vistas/js/gestorSlide.js"></script>
 
 </body>
 </html>
